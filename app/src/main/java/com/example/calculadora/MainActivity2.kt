63b1ae90a93e44d7df97b5017e20c1f5f35e169d
Jorge Pérez <77463563+per18364@users.noreply.github.com>
2021-02-04 08:06:19 -0600

    Borro de una linea sin utilizar

--- app/src/main/java/com/example/calculadora/MainActivity2.kt
+++ app/src/main/java/com/example/calculadora/MainActivity2.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
